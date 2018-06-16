e90fb10bffb2dd6fcdef69248dcdc48b83b1243a
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-06-16 16:46:00 +0700

    Add files via upload

---
+++ SumOfSquareNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        for i in range(0, int(math.sqrt(c))+1):
+            temp = c - i * i
+            j = int(math.sqrt(temp))
+            if i*i + j*j == c:
+                return True
+        return False
+        
