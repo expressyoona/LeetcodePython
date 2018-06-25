2c5ffc212c1aef28323b04ae37585086d1f64a48
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-06-25 23:56:42 +0700

    Add files via upload

---
+++ PeakIndexInMoutainArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        i, Max = 0, A[0]
+        for num in range(1, len(A)):
+            if A[num] > Max:
+                Max = A[num]
+                i = num
+        return i
+        
