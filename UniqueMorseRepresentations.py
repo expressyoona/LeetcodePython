0220c3f093cd6cab4b99f86c98151081ded24a15
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-06-26 17:10:16 +0700

    Don't exist in list

--- UniqueMorseRepresentations.py
+++ UniqueMorseRepresentations.py
@@ -1,9 +1,5 @@
 class Solution:
     def uniqueMorseRepresentations(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
         code = {'a':".-",'b':"-...",'c':"-.-.",'d':"-..",'e':".",'f':"..-.",'g':"--.",'h':"....",'i':"..",'j':".---",'k':"-.-",'l':".-..",'m':"--",'n':"-.",'o':"---",'p':".--.",'q':"--.-",'r':".-.",'s':"...",'t':"-",'u':"..-",'v':"...-",'w':".--",'x':"-..-",'y':"-.--",'z':"--.."}
         L = []
         for i in range(0, len(words)):
