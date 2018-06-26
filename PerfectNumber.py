8d5e70f8c98a8275fd70fad4f9becb60f1a1c9b3
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-06-26 15:50:56 +0700

    Add files via upload

---
+++ PerfectNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        L = {6, 28, 496, 8128, 33550336}
+        return num in L
+        
