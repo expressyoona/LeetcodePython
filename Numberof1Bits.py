fd2e853689c45bc7081e266bf769edabc1acd313
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-06-26 17:06:31 +0700

    Add files via upload

--- Numberof1Bits.py
+++ Numberof1Bits.py
@@ -4,9 +4,5 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        ctr = 0
-        while n != 0:
-            if n % 2 == 1:
-                ctr += 1
-            n = n // 2
-        return ctr
+        return bin(n).count('1')
+        
