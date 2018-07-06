eb330722799d75cce37e554616f4ae0485e831f3
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-07-06 16:40:46 +0700

    Add files via upload

---
+++ TwoSumII-Inputarrayissorted.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l = 0
+        r = len(numbers) - 1
+        while True:
+            if numbers[l] + numbers[r] == target:
+                return [l + 1, r + 1]
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            if numbers[l] + numbers[r] < target:
+                l += 1
+        return None
