62e522f3cdd642a6154c1c2f050e07f7db915f41
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2018-07-06 17:18:54 +0700

    Add files via upload

---
+++ NextGreaterElementI.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        L = []
+        for i in range(len(nums1)):
+            for j in range(nums2.index(nums1[i]), len(nums2)):
+                if nums2[j] > nums1[i]:
+                    L.append(nums2[j])
+                    break
+            else:
+                L.append(-1)
+        return L
